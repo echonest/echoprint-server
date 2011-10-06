4d341869e660b70c7a95a8b15d1ad708bbb27537
Alastair Porter <alastair@echonest.com>
2011-10-06 17:46:56 -0400

    call metadata method with correct parameter
    fixes gh-14

--- API/fp.py
+++ API/fp.py
@@ -229,7 +229,7 @@ def best_match_for_query(code_string, elbow=10, local=False):
                 logger.info("top_score > original_scores[%s]/2 (%d > %d) GOOD_MATCH_DECREASED",
                     top_track_id, top_score, original_scores[top_track_id]/2)
                 trid = top_track_id.split("-")[0]
-                meta = metadata_for_track_id(trackid)
+                meta = metadata_for_track_id(trid)
                 return Response(Response.MULTIPLE_GOOD_MATCH_HISTOGRAM_DECREASED, TRID=trid, score=top_score, qtime=response.header["QTime"], tic=tic, metadata=meta)
             else:
                 logger.info("top_score NOT > original_scores[%s]/2 (%d <= %d) BAD_HISTOGRAM_MATCH",
