714aebd659cc1977866b30ee806ef58b0c89a4e1
Andrew Nesbit <anesbit@echonest.com>
2012-03-06 06:06:52 -0500

    Recompute length of code after cutting it.

--- API/fp.py
+++ API/fp.py
@@ -157,6 +157,7 @@ def best_match_for_query(code_string, elbow=10, local=False):
         return Response(Response.NOT_ENOUGH_CODE, tic=tic)
 
     code_string = cut_code_string_length(code_string)
+    code_len = len(code_string.split(" ")) / 2
 
     # Query the FP flat directly.
     response = query_fp(code_string, rows=30, local=local, get_data=True)
